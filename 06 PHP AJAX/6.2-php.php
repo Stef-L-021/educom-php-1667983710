fffb65ed645469fca40435135fcf038d683718d5
Stef Lipka <steflipka021@gmail.com>
2022-11-25 15:59:52 +0100

    PHP AJAX examples (#6)

--- 06 PHP AJAX/6.2-php.php
+++ 06 PHP AJAX/6.2-php.php
@@ -1,3 +1,57 @@
 <?php
 
-// enter hier
+// array with names;
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophellia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+
+// get the q parameter from url
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// loopup all hints from array if $q is different from ""
+
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint ==="") {
+                $hint = $name;
+            } else {
+                $hint = ", $name";
+            }
+        } // einde stristr functie
+    } // Einde foreach
+} // Einde 1ste if
+
+// output "no suggestion"if no hint was found or output correct value
+echo $hint === "" ? "no suggestion" : $hint;
